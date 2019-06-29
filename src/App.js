fe3c3c35ebd32af4e8a8ae1131569116b3f06ab6
Juuh Aintablian <juuh.ian@gmail.com>
2019-06-29 16:52:52 -0300

    arrumando a bosta

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Menu = () => {
+  const [open, setOpen] = useState(false);
+  const toggleMenu = () => setOpen(!open);
+
+  useEffect(() => {
+    const menu = document.querySelector(".menu");
+    if (open) {
+      menu.classList.add('show');
+    } else {
+      menu.classList.remove('show');
+    }
+  }, [open])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={toggleMenu} className={`botao`}/>
+      <div className={`menu`}>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default Menu;
