b9d4d0fbc00615f90207a9b4687031c1f6405abb
Juuh Aintablian <juuh.ian@gmail.com>
2019-06-29 14:50:27 -0300

    aula semana 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +21,29 @@ function App() {
       </header>
     </div>
   );
+}*/
+
+const Menu = () => {
+  const [open, setOpen] = useState(false);
+  const toggleMenu = () => setOpen(!open);
+
+  useEffect(() => {
+    const menu = document.querySelector(".menu");
+    if (open) {
+      menu.classList.add('show');
+    } else {
+      menu.classList.remove('show');
+    }
+  }, [open])
+
+  return (
+    <div>
+      <button onClick={toggleMenu} className={`botao`}/>
+      <div className={`menu`}>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default Menu;
+//export default App;
