142e9c73e3ddb39e506f2c4637ec0a463a7b2f2c
Juuh Aintablian <juuh.ian@gmail.com>
2019-06-29 16:43:17 -0300

    tentando

--- src/App.js
+++ src/App.js
@@ -2,27 +2,6 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}*/
-
 const Menu = () => {
   const [open, setOpen] = useState(false);
   const toggleMenu = () => setOpen(!open);
@@ -46,4 +25,3 @@ const Menu = () => {
 }
 
 export default Menu;
-//export default App;
