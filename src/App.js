7f9ac47fe2b5693e58a2f8f8d417e0bf6e9f67db
Juuh Aintablian <juuh.ian@gmail.com>
2019-06-29 15:59:58 -0300

    iajsaihs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Cards from './Cards.js'
+const API_URL = "https://jsonplaceholder.typicode.com/posts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // estado inicial
+  state = {
+    posts: [],
+  }
+
+  componentDidMount() {
+    fetch(API_URL)
+    .then(result => {return result.json()})
+    .then(data => {
+      this.setState({
+        posts: data
+      })
+    })
+  }
+
+  render() {  
+    console.log(this.state.posts);
+    return (
+      <div>
+        <Cards posts={this.state.posts} />
+      </div>
+    )
+  }
+
 }
 
 export default App;
